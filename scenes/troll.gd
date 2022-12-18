94ad8c9c568c909e14e4357ebfeeae3955d7b460
aka 7D <miuatoro.exe@gmail.com>
2022-12-18 09:16:31 +0300

    Fixed Mixed Tabs Error v3.5.1
    an Error Faced me any My Team with Godot v3.5.1
    
    Tabs and spaced were mixed so i edited that, i didn't delete any comments or change any command in the code

--- scenes/troll.gd
+++ scenes/troll.gd
@@ -7,48 +7,47 @@ var _target
 
 
 func _ready():
-  _pick_random_position()
-  $body.play("run")
+	_pick_random_position()
+	$body.play("run")
 
 
 func _process(delta):
-  if self.position.distance_to(_target) > 1 :
-    var direction = self.position.direction_to(_target)
-    if direction.x > 0:
-      turn_right()
-    else:
-      turn_left()
-
-    # warning-ignore:return_value_discarded
-    move_and_collide(direction * delta * 100)
-  else:
-    $body.play("idle")
-    $rest_timer.start()
-    set_process(false)
+	if self.position.distance_to(_target) > 1 :
+		var direction = self.position.direction_to(_target)
+		if direction.x > 0:
+			turn_right()
+		else:
+			turn_left()
+	# warning-ignore:return_value_discarded
+		move_and_collide(direction * delta * 100)
+	else:
+		$body.play("idle")
+		$rest_timer.start()
+		set_process(false)
 
 
 func _pick_random_position():
-  randomize()
-  _target = Vector2(randi() % 445 + 5, randi() % 245 + 5)
+	randomize()
+	_target = Vector2(randi() % 445 + 5, randi() % 245 + 5)
 
 
 func _on_rest_timer_timeout():
-  _pick_random_position()
-  $body.play("run")
-  set_process(true)
+	_pick_random_position()
+	$body.play("run")
+	set_process(true)
 
 
 func turn_right():
-  if not $body.flip_h:
-    return
+	if not $body.flip_h:
+		return
 
-  $body.flip_h = false
-  $RayCast2D.cast_to *= -1
+	$body.flip_h = false
+	$RayCast2D.cast_to *= -1
 
 
 func turn_left():
-  if $body.flip_h:
-    return
+	if $body.flip_h:
+		return
 
-  $body.flip_h = true
-  $RayCast2D.cast_to *= -1
+	$body.flip_h = true
+	$RayCast2D.cast_to *= -1
